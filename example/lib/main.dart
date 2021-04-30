24075ca5c613678cd5e955e85ee581b1cbb9cca5
Stanislav Ilin <youngfrezyx@gmail.com>
2021-04-30 19:04:03 +0500

    configure version 0.1.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,60 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:json_view/json_view.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(JsonViewApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class JsonViewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Json View Theme',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({
-    Key key,
-  }) : super(key: key);
-
-  final open = '[';
-  final close = ']';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 60.0, left: 10, right: 10),
-        child: JsonView.asset(
-          'assets/test.json',
-          // JsonView.string(
-          // '{"team":{"value": "New", "onclick": "CreateDoc()"}}',
-          // '["Sunday", "Monday", "Tuesday", {"team":{"value1": "New","value2": "New","value3": "New","value4": "New","value5": "New","value6": "New","value7": "New","value8": "New","value9": "New", "onclick": "CreateDoc()"}}, "Thursday", "Friday", ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]]',
-          // open +
-          //     '{"employees":[{"name":"Shyam", "email":"shyamjaiswal@gmail.com"},{"name":"Bob", "email":"bob32@gmail.com"},{"name":"Jai", "email":"jai87@gmail.com"}]},' *
-          //         50 +
-          //     '{"employees":[{"name":"Shyam", "email":"shyamjaiswal@gmail.com"},{"name":"Bob", "email":"bob32@gmail.com"},{"name":"Jai", "email":"jai87@gmail.com"}]}' +
-          //     close,
-          theme: JsonViewTheme(
-              // stringStyle: TextStyle(
-              //   color: Colors.amber,
-              // ),
-              // boolStyle: TextStyle(
-              //   color: Colors.red,
-              // ),
-              // intStyle: TextStyle(
-              //   color: Colors.green,
-              // ),
-              // doubleStyle: TextStyle(
-              //   color: Colors.purple,
-              // ),
-              ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('json_view 0.1.0')),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: JsonView.string(
+            '{"people":{"name": "Stas", "lastName": "Ilin", "githubLogin": "Frezyx", "age": 19, "man": true, "height": 186}}',
+          ),
         ),
       ),
     );
