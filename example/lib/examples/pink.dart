ef071bcc1284248cc7afaffbdd237cda364047c0
Stanislav Ilin <youngfrezyx@gmail.com>
2021-04-30 21:30:14 +0500

    Create pink example

---
+++ example/lib/examples/pink.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:json_view/json_view.dart';
+
+void main() {
+  runApp(JsonViewApp());
+}
+
+class JsonViewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Json View Theme',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('json_view 0.1.0')),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: JsonView.string(
+            '{"people":{"name": "Stas", "lastName": "Ilin", "githubLogin": "Frezyx", "age": 19, "man": true, "height": 186}}',
+            theme: JsonViewTheme(
+              keyStyle: TextStyle(
+                color: Colors.deepPurple,
+              ),
+              stringStyle: TextStyle(
+                color: Colors.pink,
+              ),
+              intStyle: TextStyle(
+                color: Colors.purple,
+              ),
+              doubleStyle: TextStyle(
+                color: Colors.purple,
+              ),
+              boolStyle: TextStyle(
+                color: Colors.purple,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
