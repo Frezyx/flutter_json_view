818eea4473e929f61ea59f32247b2df53bdfc9f4
Stanislav Ilin <youngfrezyx@gmail.com>
2021-05-02 21:29:11 +0500

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,7 +18,7 @@ class JsonViewApp extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(10),
           child: JsonView.string(
-            '{"people":{"name": "Stas", "lastName": "Ilin", "githubLogin": "Frezyx", "age": 19, "man": true, "height": 186}}',
+            '{"author":{"name": "Stas", "lastName": "Ilin", "githubLogin": "Frezyx", "age": 19, "man": true, "height": 186.5}}',
           ),
         ),
       ),
